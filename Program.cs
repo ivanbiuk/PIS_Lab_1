1498775f5fb234565c29c42d417e23da8c23f6ab
ivanbiuk <ivan.biuk10@gmail.com>
2023-03-15 17:15:04 +0100

    Kebab change.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PIS_Lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World KEBAB CASE!");
         }
     }
 }
